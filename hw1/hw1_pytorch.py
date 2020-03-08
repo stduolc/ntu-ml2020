e3008979658673fb8af1aef68864e279131046ca
stduolc <stduolc@grepcode.cn>
2020-03-08 20:39:53 +0800

    add linear model of pytorch

--- hw1/hw1_pytorch.py
+++ hw1/hw1_pytorch.py
@@ -42,8 +42,12 @@ print("# normalization")
 #    for j in range(x.shape[1]):
 #        if not std[j] == 0 :
 #            x[i][j] = (x[i][j]- mean[j]) / std[j]
-x_data = Variable(torch.Tensor(x))
-y_data = Variable(torch.Tensor(y))
+
+cuda0 = torch.device('cuda:0')
+#x_data = Variable(torch.Tensor(x, device=cuda0))
+x_data = torch.from_numpy(x).float().to(cuda0)
+#y_data = Variable(torch.Tensor(y, device=cuda0))
+y_data = torch.from_numpy(y).float().to(cuda0)
 
 print('x_shape: {}'.format(x_data.shape))
 print('y_shape: {}'.format(y_data.shape))
@@ -67,10 +71,15 @@ class LinearRegressionModel(torch.nn.Module):
 
 our_model = LinearRegressionModel()
 
-criterion = torch.nn.MSELoss(size_average = False)
-optimizer = torch.optim.SGD(our_model.parameters(), lr = 0.01)
+if torch.cuda.is_available():
+    print('#use cuda')
+    our_model.cuda()
 
-for epoch in range(10):
+criterion = torch.nn.MSELoss(size_average = False)
+#optimizer = torch.optim.Adam(our_model.parameters(), lr = 0.01)
+optimizer = torch.optim.Adam(our_model.parameters(), lr = 0.01)
+
+for epoch in range(10000):
 
     # Forward pass: Compute predicted y by passing
     # x to the model
@@ -84,46 +93,30 @@ for epoch in range(10):
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
-    #print('pred_y: {}'.format(pred_y))
-    #print('y_data: {}'.format(y_data))
-    print('epoch {}, loss {}'.format(epoch, loss))
-
-#for T in range(10000):
-#    if(T % 500 == 0 ):
-#        print("T=",T)
-#        print("Loss:",np.power(np.sum(np.power(x.dot(w) - y, 2 ))/ x.shape[0],0.5))
-#    gradient = (-2) * np.transpose(x).dot(y-x.dot(w))
-#    adagrad_sum += gradient ** 2
-#    w = w - learning_rate * gradient / (np.sqrt(adagrad_sum) + 0.0005)
-#
-#np.save('weight.npy',w)     ## save weight
+    print('epoch {}, loss {}'.format(epoch, loss)) if not epoch % 1000 else False
 
 print("# read in testing set")
-#w = np.load('weight.npy')                                   ## load weight
-#test_raw_data = np.genfromtxt(sys.argv[2], delimiter=',')   ## test.csv
-#test_data = test_raw_data[:, 2: ]
-#where_are_NaNs = np.isnan(test_data)
-#test_data[where_are_NaNs] = 0
+test_raw_data = np.genfromtxt(sys.argv[2], delimiter=',')   ## test.csv
+test_data = test_raw_data[:, 2: ]
+where_are_NaNs = np.isnan(test_data)
+test_data[where_are_NaNs] = 0
 
 print("# predict")
-#test_x = np.empty(shape = (240, 18 * 9),dtype = float)
-#
-#for i in range(240):
-#    test_x[i,:] = test_data[18 * i : 18 * (i+1),:].reshape(1,-1)
-#
-#for i in range(test_x.shape[0]):        ##Normalization
-#    for j in range(test_x.shape[1]):
-#        if not std[j] == 0 :
-#            test_x[i][j] = (test_x[i][j]- mean[j]) / std[j]
-#
-#test_x = np.concatenate((np.ones(shape = (test_x.shape[0],1)),test_x),axis = 1).astype(float)
-#answer = test_x.dot(w)
+test_x = np.empty(shape = (240, 18 * 9),dtype = float)
+
+for i in range(240):
+    test_x[i,:] = test_data[18 * i : 18 * (i+1),:].reshape(1,-1)
+
+x_test = torch.from_numpy(test_x).float().to(cuda0)
+
+answer = our_model(x_test)
+
 
 print("#write file")
-#f = open(sys.argv[3],"w")
-#w = csv.writer(f)
-#title = ['id','value']
-#w.writerow(title)
-#for i in range(240):
-#    content = ['id_'+str(i),answer[i][0]]
-#    w.writerow(content)
+f = open(sys.argv[3],"w")
+w = csv.writer(f)
+title = ['id','value']
+w.writerow(title)
+for i in range(240):
+    content = ['id_'+str(i),answer[i][0].item()]
+    w.writerow(content)
